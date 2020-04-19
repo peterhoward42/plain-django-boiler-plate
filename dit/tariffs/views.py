eb5378dd86824bd960abd8dc9ed9c27810ae894f
test <test@tests-MacBook-Pro.local>
2020-04-19 11:45:47 +0100

    Play around with template hierarchy (broken wip)
    This commit subclasses the main headings page template from a boiler plate
    template.

--- dit/tariffs/views.py
+++ dit/tariffs/views.py
@@ -1,11 +1,8 @@
-from django.http import HttpResponse
-
 from django.shortcuts import render
 
 from .view_model import ViewModel
 
 
-def heading(request, heading):
-    context = {"heading": heading}
+def index(request, heading):
     view_model = ViewModel(heading)
     return render(request, "tariffs/heading.html", view_model.data)
